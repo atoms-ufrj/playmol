392f4f3c994117afb46d9c278b8ff9e9c153d084
Charlles Abreu <abreu@eq.ufrj.br>
2017-02-16 23:31:11 -0200

    Inclusion of compiled html docs

---
+++ docs/html/navtreeindex0.js
@@ -0,0 +1,11 @@
+var NAVTREEINDEX0 =
+{
+"basics.html":[2],
+"citelist.html":[5],
+"commands.html":[3],
+"index.html":[],
+"index.html":[0],
+"install.html":[1],
+"pages.html":[],
+"scripts.html":[4]
+};
